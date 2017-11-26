acea1a6fa344407481123de4527475a39b066a4f
PierceGriffiths <gripwolf@gmail.com>
2017-11-26 14:12:15 -0600

    Began implementing PIN screen

---
+++ app/src/main/java/com/mohamedkevinlukepierce/budgetbuddy/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.mohamedkevinlukepierce.budgetbuddy;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.andrognito.pinlockview.IndicatorDots;
+import com.andrognito.pinlockview.PinLockListener;
+import com.andrognito.pinlockview.PinLockView;
+
+public class LoginActivity extends AppCompatActivity {
+
+}
